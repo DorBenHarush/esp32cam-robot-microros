58619a50382a1850dcd9899afbb5cff7645d3ca6
Dor Ben Harush <dor2981123@gmail.com>
2020-10-29 08:57:53 +0200

    Update app.c

--- camera/app.c
+++ camera/app.c
@@ -1,13 +1,3 @@
-/**
- * This example takes a picture every 5s.
-   Example src: https://github.com/espressif/esp32-camera.git 
- */
-
-
-
-
-// ================================ CODE ======================================
-
 #include <esp_event_loop.h>
 #include <esp_log.h>
 #include <esp_system.h>
@@ -20,38 +10,86 @@
 
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
-#include <std_msgs/msg/int32.h>
+#include <sensor_msgs/msg/compressed_image.h>
+
 
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
+
 #include "boards.h"
 
+#define STRING_CAPACITY 20
+#define GPIO_PWM0A_OUT 15   //Set GPIO 15 as PWM0A
+#define GPIO_PWM0B_OUT 16   //Set GPIO 16 as PWM0B
 
+#define GPIO_PWM1A_OUT 13   //Set GPIO 13 as PWM1A
+#define GPIO_PWM1B_OUT 14   //Set GPIO 1 as PWM1B
 
-
-static const char *TAG = "example:take_picture";
-
+static const char *TAG = "stream_pictures";
 
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
+//ROS and camera variables
+
 rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
+sensor_msgs__msg__CompressedImage* msg;
 camera_fb_t *pic;
 
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+//MCPWM config
+mcpwm_config_t pwm_0_config;
+mcpwm_config_t pwm_1_config;
+
+static void mcpwm_gpio_initialize(void)
 {
-	UNUSED(last_call_time);
-	if (timer != NULL) {
-		RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-		// use pic->buf to access the image
-		msg.data = pic->len;
-	}
+    printf("Initializing gpio. \n");
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, GPIO_PWM0A_OUT);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, GPIO_PWM0B_OUT);
+    mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM1A, GPIO_PWM1A_OUT);
+    mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM1B, GPIO_PWM1B_OUT);
 }
+
+
+
+static void brushed_motor_forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle)
+{
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);
+    mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, duty_cycle);
+    mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_A, MCPWM_DUTY_MODE_0); //call this each time, if operator was previously in low/high state
+}
+
+
+static void brushed_motor_backward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle)
+{
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_A);
+    mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_B, duty_cycle);
+    mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_B, MCPWM_DUTY_MODE_0);  //call this each time, if operator was previously in low/high state
+}
+
+
+static void brushed_motor_stop(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num)
+{
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_A);
+    mcpwm_set_signal_low(mcpwm_num, timer_num, MCPWM_OPR_B);
+}
+
+
+static void initialize_parameters_of_mcpwm(mcpwm_config_t* pwm_config, bool unit)
+{
+	printf("Initializing prameters of MCPWM. \n");	
+	pwm_config->frequency = 1000;    //frequency = 500Hz,
+	pwm_config->cmpr_a = 0;    //duty cycle of PWMxA = 0
+	pwm_config->cmpr_b = 0;    //duty cycle of PWMxb = 0
+	pwm_config->counter_mode = MCPWM_UP_COUNTER;
+	pwm_config->duty_mode = MCPWM_DUTY_MODE_0;
+	(unit == 0) ? mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, pwm_config) : mcpwm_init(MCPWM_UNIT_1, MCPWM_TIMER_1, pwm_config); //Configure PWM0A & PWM0B with above settings
+}
+
 
 
 static camera_config_t camera_config = {
@@ -79,11 +117,19 @@ static camera_config_t camera_config = {
     .ledc_channel = LEDC_CHANNEL_0,
 
     .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
-    .frame_size = FRAMESIZE_VGA,    //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+    .frame_size = FRAMESIZE_HVGA,    //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
 
     .jpeg_quality = 12, //0-63 lower number means higher quality
     .fb_count = 1       //if more than one, i2s runs in continuous mode. Use only with JPEG
 };
+
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+	UNUSED(last_call_time);
+	if (timer != NULL) {
+		
+	}
+}
 
 static esp_err_t init_camera()
 {
@@ -91,41 +137,60 @@ static esp_err_t init_camera()
     esp_err_t err = esp_camera_init(&camera_config);
     if (err != ESP_OK)
     {
-        ESP_LOGE(TAG, "Camera Init Failed");
+        ESP_LOGE(TAG, "Camera Init Failed.");
+		
         return err;
     }
 
     return ESP_OK;
 }
 
+
+
+void moveMotors()
+{
+	//init mcpwm for motors
+	mcpwm_gpio_initialize();
+	initialize_parameters_of_mcpwm(&pwm_0_config, 0);
+	initialize_parameters_of_mcpwm(&pwm_1_config, 1);
+	//activate motors
+	brushed_motor_forward(MCPWM_UNIT_0, MCPWM_TIMER_0, 50.0);
+	brushed_motor_forward(MCPWM_UNIT_1, MCPWM_TIMER_1, 50.0);
+	while(1);
+	//stop motors
+	brushed_motor_stop(MCPWM_UNIT_0, MCPWM_TIMER_0);
+	brushed_motor_stop(MCPWM_UNIT_1, MCPWM_TIMER_1);
+	vTaskDelete(NULL);
+
+}
+
+
+
 void appMain(void * arg)
 {
-    init_camera();
-    rcl_allocator_t allocator = rcl_get_default_allocator();
+	//init camera	
+	init_camera();
+
+	
+
+	rcl_allocator_t allocator = rcl_get_default_allocator();
 	rclc_support_t support;
+	msg = sensor_msgs__msg__CompressedImage__create();
 
 	// create init_options
 	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
 
 	// create node
 	rcl_node_t node = rcl_get_zero_initialized_node();
-	RCCHECK(rclc_node_init_default(&node, "freertos_pictureSize_publisher", "", &support));
+	RCCHECK(rclc_node_init_default(&node, "freertos_picture_publisher", "", &support));
 
 	// create publisher
-	RCCHECK(rclc_publisher_init_default(
-		&publisher,
-		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-		"freertos_pictureSize_publisher"));
+	RCCHECK(rclc_publisher_init_default(&publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, CompressedImage), "freertos_picture_publisher"));
 
 	// create timer,
 	rcl_timer_t timer = rcl_get_zero_initialized_timer();
 	const unsigned int timer_timeout = 1000;
-	RCCHECK(rclc_timer_init_default(
-		&timer,
-		&support,
-		RCL_MS_TO_NS(timer_timeout),
-		timer_callback));
+	RCCHECK(rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(timer_timeout), timer_callback));
 
 	// create executor
 	rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
@@ -135,18 +200,64 @@ void appMain(void * arg)
 	RCCHECK(rclc_executor_set_timeout(&executor, RCL_MS_TO_NS(rcl_wait_timeout)));
 	RCCHECK(rclc_executor_add_timer(&executor, &timer));
 
-	msg.data = 0;
+	
+	//set pixel format and frame id
+	char * encoding = "jpeg";
+	msg->format.data = (char*)heap_caps_malloc(strlen(encoding) + 1, MALLOC_CAP_8BIT); 
+	msg->format.capacity = strlen(encoding) + 1;
+	memset(msg->format.data, 0, strlen(encoding) + 1);
+	memcpy(msg->format.data, encoding, strlen(encoding));
+	msg->format.size = strlen(encoding) + 1;
 
-	while(1){
-		rclc_executor_spin_some(&executor, 100);
+	char* frame_id = "AI_thinker_image";
+	msg->header.frame_id.data = (char*)heap_caps_malloc(strlen(frame_id) + 1, MALLOC_CAP_8BIT);
+	msg->header.frame_id.capacity = strlen(encoding) + 1;
+	memset(msg->header.frame_id.data, 0, strlen(frame_id) + 1);
+	memcpy(msg->header.frame_id.data, frame_id, strlen(frame_id));
+	msg->header.frame_id.size = strlen(frame_id) + 1;
+
+	//activate motors
+	brushed_motor_forward(MCPWM_UNIT_0, MCPWM_TIMER_0, 50.0);
+	brushed_motor_forward(MCPWM_UNIT_1, MCPWM_TIMER_1, 50.0);
+
+
+	while(1)
+	{
+		rclc_executor_spin_some(&executor, 1000);
 		usleep(100000);
-		//takes picture
-        pic = esp_camera_fb_get();
-       	vTaskDelay(5000 / portTICK_RATE_MS);
+		
+		pic = esp_camera_fb_get();
+		//allocate memory for data
+		msg->data.data = (uint8_t*)heap_caps_realloc(msg->data.data, pic->len, MALLOC_CAP_8BIT);
+		msg->data.capacity = pic->len;
+		msg->data.size = pic->len;
+		//check for NULL ptr
+		if(msg->data.data == NULL)
+		{
+			printf("Failed to allocate memory. restating in 5 sec.\n");
+			vTaskDelay(5000 / portTICK_RATE_MS);
+			esp_restart();
+		}
+		printf("Successfully allocated %d bytes. copying...\n", pic->len);
+		//copy data
+		memcpy(msg->data.data, pic->buf, pic->len);
+		//print msg info
+		printf("Pixel format %s \n", msg->format.data);
+		printf("Frame id %s \n", msg->header.frame_id.data);
+		printf("Buffer length %d \n", msg->data.size);
+		//publish picture
+		printf("Publisher return value: %d \n", rcl_publish(&publisher, msg, NULL));
+		vTaskDelay(200 / portTICK_RATE_MS);
+
 	}
 
-    // free resources
+
+	// free resources
 	RCCHECK(rcl_publisher_fini(&publisher, &node))
 	RCCHECK(rcl_node_fini(&node))
-    vTaskDelete(NULL);
+	heap_caps_free(msg->format.data);
+	heap_caps_free(msg->header.frame_id.data);
+	heap_caps_free(msg->data.data);
+	sensor_msgs__msg__CompressedImage__destroy(msg);
+	vTaskDelete(NULL);
 }
